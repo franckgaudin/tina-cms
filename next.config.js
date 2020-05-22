3223dbdef19380d73f3e90e6a2190eeee096bd24
Franck Gaudin <franck@franckgaudin.com>
2020-05-22 06:22:30 -0400

    github config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,12 @@
-require('dotenv').config();
-
 const isProd = (process.env.NODE_ENV || 'production') === 'production'
 
+require('dotenv').config({ path: isProd ? '.env.production' : '.env.local' } );
+
 module.exports = {
+  assetPrefix: isProd ? '/tina-cms' : '',
   env: {
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     REPO_FULL_NAME: process.env.REPO_FULL_NAME,
     BASE_BRANCH: process.env.BASE_BRANCH,
   },
-  exportPathMap: () => ({
-    '/': { page: '/' },
-  }),
-  assetPrefix: isProd ? '/tina-cms' : '',
 } 
